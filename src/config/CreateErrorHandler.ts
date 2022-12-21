1fd8e16664b60ca1e43d7f259bb763c032069ceb
lucasSGomide <lucas.sgomide92@gmail.com>
2022-12-21 14:21:07 -0300

    feat: add invalidPasswordError to handler build

--- src/config/CreateErrorHandler.ts
+++ src/config/CreateErrorHandler.ts
@@ -2,11 +2,13 @@ import { ErrorsHandler } from '../application/errorHandler/ErrorsHandler'
 import {
     BadRequestErrorHandler,
     NotFoundErrorHandler,
+    InvalidPasswordErrorHandler,
 } from '../application/handlers'
 
 export function createErrorHandler() {
     return ErrorsHandler.build([
         new BadRequestErrorHandler(),
         new NotFoundErrorHandler(),
+        new InvalidPasswordErrorHandler(),
     ])
 }
