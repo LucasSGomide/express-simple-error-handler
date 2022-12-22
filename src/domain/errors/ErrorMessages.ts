41a6137d2229a82cbd5512473e0a91065b003587
lucasSGomide <lucas.sgomide92@gmail.com>
2022-12-22 18:12:03 -0300

    feat: add unauthorized error name, status and message

--- src/domain/errors/ErrorMessages.ts
+++ src/domain/errors/ErrorMessages.ts
@@ -2,4 +2,5 @@ export const errorMessages = {
     badRequest: 'Invalid request.',
     notFound: 'Resource not found.',
     invalidPassword: 'Invalid password.',
+    unauthorized: 'Invalid credentials.',
 }
